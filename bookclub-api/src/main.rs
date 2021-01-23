8d3ff6983e7a394c5c9bbd18334b4f3371f2d915
Martin Disch <martindisch@gmail.com>
2021-01-23 16:57:32 +0100

    Set up actix-web

--- bookclub-api/src/main.rs
+++ bookclub-api/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+use std::io::Result;
+
+#[get("/v1/meetings")]
+async fn meetings() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| App::new().service(meetings))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
