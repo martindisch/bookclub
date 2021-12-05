f2fc8464496a3156e002dd010fa5c811b8984cb1
Martin Disch <martindisch@gmail.com>
2021-12-05 16:57:45 +0100

    Sketch up first component

---
+++ bookclub-client/src/components/Book.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let title: string;
+  export let author: string;
+  export let description: string;
+  export let pageCount: number;
+  export let pitchBy: string;
+  export let firstSuggested: Date;
+  export let supporters: string[];
+</script>
+
+<h1>{title}</h1>
+<p>{author}</p>
+<p>{description}</p>
+<p>{pageCount}</p>
+<p>{pitchBy}</p>
+<p>{firstSuggested}</p>
+<p>{supporters}</p>
--- bookclub-client/src/routes/index.svelte
+++ bookclub-client/src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Book from "../components/Book.svelte";
+</script>
+
+<Book
+  title="Hello, world!"
+  author="Brian Kernighan"
+  pageCount={200}
+  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, augue quis sagittis egestas, nulla felis faucibus odio, vel vehicula metus nunc a justo. Proin elementum sapien sapien, fermentum ornare massa pellentesque non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam accumsan risus non leo sollicitudin, sit amet pharetra augue cursus. Curabitur rutrum faucibus diam non volutpat. Mauris tincidunt a orci eu euismod. Morbi convallis finibus justo non porta. Sed nec efficitur risus, in volutpat lacus. Nam eleifend efficitur sapien in egestas. Aliquam cursus auctor mi eu semper. Maecenas consectetur nec velit nec efficitur. Ut dapibus metus ut congue tempor. Nunc faucibus, nisi egestas ornare euismod, velit tellus egestas magna, eu vehicula ante nunc non dui. Etiam congue purus felis, vitae mattis arcu tincidunt eget. Integer consequat fermentum nulla, in porta eros."
+  pitchBy="Martin"
+  firstSuggested={new Date("2021-12-05T16:53:25Z")}
+  supporters={["Martin", "Alice", "Bob"]}
+/>
