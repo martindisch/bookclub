08c76376b40e2761822e33ff3127505501e5bca8
Martin Disch <martindisch@gmail.com>
2021-12-06 20:13:26 +0100

    Add vote button

--- bookclub-client/src/components/Book.svelte
+++ bookclub-client/src/components/Book.svelte
@@ -14,9 +14,15 @@
 
   <p class="mb-4 hyphens-auto text-justify">{description}</p>
 
-  <div class="grid grid-cols-2">
+  <div class="grid grid-cols-3">
     <p>Page count</p>
     <p>{pageCount}</p>
+    <div class="row-span-4 flex justify-end items-end">
+      <button
+        class="bg-green-700 hover:bg-green-800 active:bg-green-700 py-2 px-8 rounded-md shadow focus:outline-none"
+        >Vote</button
+      >
+    </div>
     <p>Pitch by</p>
     <p>{pitchBy}</p>
     <p>First suggested</p>
