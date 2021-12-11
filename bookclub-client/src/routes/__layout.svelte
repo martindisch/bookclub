12ffa88051f88954440f8900a286e09ee7f5bb1d
Martin Disch <martindisch@gmail.com>
2021-12-11 17:03:27 +0100

    Make background fill at least viewport

--- bookclub-client/src/routes/__layout.svelte
+++ bookclub-client/src/routes/__layout.svelte
@@ -2,6 +2,6 @@
   import "virtual:windi.css";
 </script>
 
-<div class="h-full bg-gray-900 p-6 text-gray-100 flex justify-center">
+<div class="min-h-screen bg-gray-900 p-6 text-gray-100 flex justify-center">
   <div class="max-w-screen-md"><slot /></div>
 </div>
