196a9ca94da089a3c491ba7202556d69f7fa766c
Martin Disch <martindisch@gmail.com>
2021-12-11 17:56:36 +0100

    Make container full width

--- bookclub-client/src/routes/__layout.svelte
+++ bookclub-client/src/routes/__layout.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="min-h-screen bg-gray-900 p-6 text-gray-100 flex justify-center">
-  <div class="max-w-screen-md"><slot /></div>
+  <div class="max-w-screen-md w-full"><slot /></div>
 </div>
