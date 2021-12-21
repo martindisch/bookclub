c8a5645608f17f3894209803e9e0b1be74a06a7f
Martin Disch <martindisch@gmail.com>
2021-12-21 18:29:27 +0100

    Load books from API

--- bookclub-client/src/routes/index.svelte
+++ bookclub-client/src/routes/index.svelte
@@ -1,42 +1,56 @@
+<script lang="ts" context="module">
+  import type { Load } from "@sveltejs/kit";
+
+  interface BookResponse {
+        id: string,
+        title: string,
+        author: string,
+        description: string,
+        pageCount: number,
+        pitchBy: string,
+        firstSuggested: string,
+        supporters: string[]
+  }
+
+  export const load: Load = async ({ page, fetch }) => {
+    const res = await fetch(`${import.meta.env.VITE_API}/v1/books`);
+    if (res.ok) {
+      return {
+        props: {
+          books: await res.json(),
+        },
+      };
+    }
+    return {
+      status: res.status,
+      error: new Error("Could not load books"),
+    };
+  };
+</script>
+
 <script lang="ts">
   import { goto } from "$app/navigation";
   import Book from "../components/Book.svelte";
   import List from "../components/List.svelte";
   import Button from "../components/Button.svelte";
 
+  export let books: BookResponse[];
+
   const vote = () => alert("Voted for a book");
 </script>
 
 <List>
+{#each books as book (book.id)}
   <Book
-    title="Hello, world!"
-    author="Brian Kernighan"
-    pageCount={200}
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, augue quis sagittis egestas, nulla felis faucibus odio, vel vehicula metus nunc a justo. Proin elementum sapien sapien, fermentum ornare massa pellentesque non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam accumsan risus non leo sollicitudin, sit amet pharetra augue cursus. Curabitur rutrum faucibus diam non volutpat. Mauris tincidunt a orci eu euismod. Morbi convallis finibus justo non porta. Sed nec efficitur risus, in volutpat lacus. Nam eleifend efficitur sapien in egestas. Aliquam cursus auctor mi eu semper. Maecenas consectetur nec velit nec efficitur. Ut dapibus metus ut congue tempor. Nunc faucibus, nisi egestas ornare euismod, velit tellus egestas magna, eu vehicula ante nunc non dui. Etiam congue purus felis, vitae mattis arcu tincidunt eget. Integer consequat fermentum nulla, in porta eros."
-    pitchBy="Martin"
-    firstSuggested={new Date("2021-12-05T16:53:25Z")}
-    supporters={["Martin", "Alice", "Bob"]}
+    title={book.title}
+    author={book.author}
+    pageCount={book.pageCount}
+    description={book.description}
+    pitchBy={book.pitchBy}
+    firstSuggested={new Date(Date.parse(book.firstSuggested))}
+    supporters={book.supporters}
     onVote={vote}
   />
-  <Book
-    title="Goodbye and thanks for all the fish"
-    author="Douglas Adams"
-    pageCount={350}
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, augue quis sagittis egestas, nulla felis faucibus odio, vel vehicula metus nunc a justo. Proin elementum sapien sapien, fermentum ornare massa pellentesque non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam accumsan risus non leo sollicitudin, sit amet pharetra augue cursus."
-    pitchBy="Alice"
-    firstSuggested={new Date("2021-12-05T16:53:25Z")}
-    supporters={["Alice"]}
-    onVote={vote}
-  />
-  <Book
-    title="A brief history of nothing"
-    author="Paterson"
-    pageCount={111}
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie, augue quis sagittis egestas, nulla felis faucibus odio, vel vehicula metus nunc a justo. Proin elementum sapien sapien, fermentum ornare massa pellentesque non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam accumsan risus non leo sollicitudin, sit amet pharetra augue cursus. Curabitur rutrum faucibus diam non volutpat. Mauris tincidunt a orci eu euismod. Morbi convallis finibus justo non porta. Sed nec efficitur risus, in volutpat lacus. Nam eleifend efficitur sapien in egestas. Aliquam cursus auctor mi eu semper. Maecenas consectetur nec velit nec efficitur. Ut dapibus metus ut congue tempor. Nunc faucibus, nisi egestas ornare euismod, velit tellus egestas magna, eu vehicula ante nunc non dui. Etiam congue purus felis, vitae mattis arcu tincidunt eget. Integer consequat fermentum nulla, in porta eros."
-    pitchBy="Bob"
-    firstSuggested={new Date("2021-12-05T16:53:25Z")}
-    supporters={["Bob", "Alice"]}
-    onVote={vote}
-  />
+{/each}
 </List>
 <Button text="+" rounded={true} on:click={() => goto("/books/new")} />
