4945e8b14e937306ee329c70897a52517dc77093
Martin Disch <martindisch@gmail.com>
2021-12-21 14:09:24 +0100

    Turn book page into specific /books/new page
    We'll only need the other one for editing functionality in the future.

---
+++ bookclub-client/src/routes/books/new.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import BookForm, { Book } from "../../components/BookForm.svelte";
+
+  export let id: number;
+  export let title: string;
+
+  const saveBook = (book: Book) => {
+    alert(JSON.stringify(book));
+  };
+</script>
+
+<BookForm onSave={saveBook} />
