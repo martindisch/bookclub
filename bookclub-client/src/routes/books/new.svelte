820aff1133000d023623cb802edefdf7dafe0f20
Martin Disch <martindisch@gmail.com>
2021-12-22 09:13:44 +0100

    Remove unused props

--- bookclub-client/src/routes/books/new.svelte
+++ bookclub-client/src/routes/books/new.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import BookForm, { Book } from "../../components/BookForm.svelte";
 
-  export let id: number;
-  export let title: string;
-
   const saveBook = (book: Book) => {
     alert(JSON.stringify(book));
   };
