1c4ebca28df1fa1376bf0bac7f96cdf2a5248423
Martin Disch <martindisch@gmail.com>
2021-12-22 16:12:50 +0100

    Implement saving book

--- bookclub-client/src/routes/books/new.svelte
+++ bookclub-client/src/routes/books/new.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
   import BookForm, { Book } from "../../components/BookForm.svelte";
+  import { goto } from "$app/navigation";
 
-  const saveBook = (book: Book) => {
-    alert(JSON.stringify(book));
+  const saveBook = async (book: Book) => {
+    await fetch(`${import.meta.env.VITE_API}/v1/books`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...book,
+        supporters: [book.pitchBy],
+      }),
+    });
+
+    goto("/");
   };
 </script>
 
