2c32a3dc6926784a6fe42243bbd7f55bfb482984
Martin Disch <martindisch@gmail.com>
2021-12-23 14:51:49 +0100

    Create modal for simple text prompt

---
+++ bookclub-client/src/components/TextModal.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Card from "./Card.svelte";
+  import InputText from "./InputText.svelte";
+  import Button from "./Button.svelte";
+
+  export let prompt: string;
+  export let value = "";
+  export let onSave: (userName: string) => void;
+</script>
+
+<div class="fixed inset-0 bg-gray-600 bg-opacity-90" />
+<div class="fixed inset-0 flex items-center justify-center w-full">
+  <Card>
+    <form on:submit|preventDefault={() => onSave(value)} class="flex flex-col gap-4">
+      <InputText label={prompt} id="input" bind:value />
+      <div class="place-self-end mt-2">
+        <Button text="Save" submit={true} />
+      </div>
+    </form>
+  </Card>
+</div>
