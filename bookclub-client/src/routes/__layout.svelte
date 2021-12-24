26777dcf15b2d894c2d8d473d021a92c9217a7a3
Martin Disch <martindisch@gmail.com>
2021-12-24 12:34:52 +0100

    Properly do favicon
    https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs

--- bookclub-client/src/routes/__layout.svelte
+++ bookclub-client/src/routes/__layout.svelte
@@ -2,6 +2,13 @@
   import "virtual:windi.css";
 </script>
 
+<svelte:head>
+  <link rel="icon" href="/favicon.ico" sizes="any" />
+  <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  <link rel="manifest" href="/manifest.webmanifest" />
+</svelte:head>
+
 <div class="min-h-screen bg-gray-900 p-6 text-gray-100 flex justify-center">
   <div class="max-w-screen-md w-full"><slot /></div>
 </div>
