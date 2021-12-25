234ccd452a69f3adc7d44475762f120f75393ae7
Martin Disch <martindisch@gmail.com>
2021-12-25 15:56:21 +0100

    Use hash link to scroll to newly created book

--- bookclub-client/src/routes/books/new.svelte
+++ bookclub-client/src/routes/books/new.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import BookForm, { Book } from "../../components/BookForm.svelte";
+  import { responseToBook } from "../../api";
   import { goto } from "$app/navigation";
 
   const saveBook = async (book: Book) => {
-    await fetch(`${import.meta.env.VITE_API}/v1/books`, {
+    const res = await fetch(`${import.meta.env.VITE_API}/v1/books`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -13,8 +14,9 @@
         supporters: [book.pitchBy],
       }),
     });
+    const newBook = responseToBook(await res.json());
 
-    goto("/");
+    goto(`/#${newBook.id}`);
   };
 </script>
 
