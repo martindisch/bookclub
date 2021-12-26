c81ee97a64703b6e2010a5495520a3403f5d3e46
Martin Disch <martindisch@gmail.com>
2021-12-26 10:07:42 +0100

    Scroll to book after editing too

--- bookclub-client/src/routes/books/[id].svelte
+++ bookclub-client/src/routes/books/[id].svelte
@@ -20,20 +20,21 @@
 <script lang="ts">
   import BookForm, { Book } from "../../components/BookForm.svelte";
   import { goto } from "$app/navigation";
-  import type { BookResponse } from "../../api";
+  import { BookResponse, responseToBook } from "../../api";
 
   export let book: BookResponse;
 
   const updateBook = async (updatedBook: Book) => {
-    await fetch(`${import.meta.env.VITE_API}/v1/books/${book.id}`, {
+    const res = await fetch(`${import.meta.env.VITE_API}/v1/books/${book.id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(updatedBook),
     });
+    const bookResponse = responseToBook(await res.json());
 
-    goto("/");
+    goto(`/#${bookResponse.id}`);
   };
 
   const deleteBook = async () => {
